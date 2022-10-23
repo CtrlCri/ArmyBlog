795c597731f94c802a125c539029d0f51c33f307
armycrih <armycrih@gmail.com>
2022-10-23 12:08:22 -0300

    env.sample and data models

--- myblog/blogapp/models.py
+++ myblog/blogapp/models.py
@@ -1,3 +1,39 @@
+#Django
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    """ django data model tag """
+    
+    name = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField("date created")
+    updated_at = models.DateTimeField("date updated")
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+    
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    """ django data model post """
+    
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    body = models.TextField('content')
+    tag = models.ManyToManyField(
+        Tag,
+        verbose_name = 'tag'
+    )
+
+    created_at = models.DateTimeField("date published")
+    updated_at = models.DateTimeField("date updated")
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return self.title
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -24,8 +24,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'blogapp'
-
+    #"blogapp.apps.BlogappConfig"
+    'blogapp'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
