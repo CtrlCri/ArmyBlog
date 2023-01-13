5e455b4e88cfba5c83a41400660fb3439024cacf
CtrlCri <aramayoreyes@gmail.com>
2023-01-13 14:07:57 -0300

    update views

--- myblog/blogapp/views.py
+++ myblog/blogapp/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def index(request):
-    return HttpResponse("Hello Blog")
+from .models import Post
+from .serializers import PostSerializer
 
 
+class BlogListView(APIView):
+    def get(self, request, *args, **kwargs):
+        posts = Post.postobjects.all()[0:4]
+        serializer = PostSerializer(posts,many=True)
+        return Response(serializer.data)
+
+
+class PostDetailView(APIView):
+    def get(self, request, post_slug,*args, **kwargs):
+        post = get_object_or_404(Post, slug=post_slug)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
